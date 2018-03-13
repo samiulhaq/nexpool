90c9e997a7025d33d30fc6bbdc56bca09f61c333
webdev2 <webdev2@NOCMAC08.local>
2018-03-13 18:53:05 +0400

    First Commit

---
+++ v2/scripts/custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+$('.btn-toggle').click(function() {
+    $(this).find('.btn').toggleClass('active');  
+    
+    if ($(this).find('.btn-primary').size()>0) {
+    	$(this).find('.btn').toggleClass('btn-primary');
+    }
+    
+    $(this).find('.btn').toggleClass('btn-default');
+       
+});
+
+$('form').submit(function(){
+    return false;
+});
